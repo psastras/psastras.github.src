caeafa831c44642b6961385411f8f3b0bd4d6230
Paul Sastrasinh <psastras@gmail.com>
2017-12-23 18:22:57 -0500

    add threejs

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { h, Component } from "preact";
 import { Nav } from "./components/Nav";
 import { Canvas } from "./components/Canvas";
 import { Main } from "./components/Main";
+import { Birds } from "./components/Birds";
 
 namespace App {
   export interface Props {}
@@ -13,6 +14,7 @@ export class App extends Component<App.Props, App.State> {
     return (
       <div>
         <Canvas />
+        <Birds />
         <div style={{ position: "absolute", zIndex: 2, width: "100vw" }}>
           <section class="hero is-fullheight">
             <Nav />
