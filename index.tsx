2be4b68088b8c47f53aa57cf9d35bd6ebed382ef
Paul Sastrasinh <psastras@gmail.com>
2018-05-28 17:04:15 -0400

    styling

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import { h, render } from "preact";
 import { App } from "./app";
-import "milligram/dist/milligram.css";
+
 import "./index.css";
 render(<App />, document.getElementById("app"), document.getElementById("app")
   .lastChild as any);
