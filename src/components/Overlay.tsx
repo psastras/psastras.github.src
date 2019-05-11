dfd5abf122c795a42bd56dfc05d4130e4825a244
Paul Sastrasinh <psastras@gmail.com>
2019-05-12 00:32:40 +0900

    fix: swap links

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -6,10 +6,10 @@ export const Overlay: React.FunctionComponent = () => (
     <div className={styles.overlayText}>
       <p className={styles.overlayTitle}>Developer</p>
       <p className={styles.overlaySubtitle}>
-        <a href="https://www.linkedin.com/in/psastras/" target="_blank">
-          GitHub
-        </a>{" "}
         <a href="http://github.com/psastras/" target="_blank">
+          Github
+        </a>{" "}
+        <a href="https://www.linkedin.com/in/psastras/" target="_blank">
           LinkedIn
         </a>
       </p>
