d1f22c60835d4efcd1254b789a5e7ed98d7694e1
Paul Sastrasinh <psastras@gmail.com>
2019-05-12 22:22:25 +0900

    feat: add insta and twitter links

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,3 +1,4 @@
+import classnames from "classnames";
 import * as React from "react";
 import * as styles from "./overlay.css";
 
@@ -11,6 +12,19 @@ export const Overlay: React.FunctionComponent = () => (
         </a>{" "}
         <a href="https://www.linkedin.com/in/psastras/" target="_blank">
           LinkedIn
+        </a>
+      </p>
+      <p
+        className={classnames(
+          styles.overlaySubtitle,
+          styles.overlaySubSubtitle
+        )}
+      >
+        <a href="https://instagram.com/psastras/" target="_blank">
+          <i className="fab fa-instagram" />
+        </a>{" "}
+        <a href="http://twitter.com/psastras" target="_blank">
+          <i className="fab fa-twitter" />
         </a>
       </p>
     </div>
--- src/components/overlay.css.d.ts
+++ src/components/overlay.css.d.ts
@@ -2,3 +2,4 @@ export const overlay: string;
 export const overlayText: string;
 export const overlayTitle: string;
 export const overlaySubtitle: string;
+export const overlaySubSubtitle: string;
